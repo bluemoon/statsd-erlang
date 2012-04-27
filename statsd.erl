574249c4241a431de74735550927a2b80e469f2c
Dominik Liebler <liebler.dominik@googlemail.com>
2012-04-27 18:13:47 +0200

    using a record to hold the state of the UDP connection

--- statsd.erl
+++ statsd.erl
@@ -1,44 +1,72 @@
 -module(statsd).
--export([increment/1, increment/2, decrement/1, decrement/2, count/2, count/3, timing/2, timing/3]).
+-author("Dominik Liebler").
+
+-export([start/2, start/1, start/0, stop/1]).
+-export([increment/2, increment/3, decrement/2, decrement/3, count/3, count/4, timing/3, timing/4]).
+
+-define(STATSD_DEFAULT_PORT, 8125).
+-define(STATSD_DEFAULT_HOST, "localhost").
+
+%% holds all the relevant state that must be passed to all functions
+-record(state, { 
+	port = ?STATSD_DEFAULT_PORT,
+	host = ?STATSD_DEFAULT_HOST,
+	socket
+}).
+
+%% Public: opens the socket
+%%
+%% returns a #state record containing the socket
+start(Host, Port) ->
+	State = #state{port = Port, host = Host},
+	{ok, Socket} = gen_udp:open(0),
+	State#state{socket = Socket}.
+start(Host) ->
+	start(Host, ?STATSD_DEFAULT_PORT).
+start() ->
+	start(?STATSD_DEFAULT_HOST).
+
+stop(State) ->
+	gen_udp:close(State#state.socket).
 
 %% Public: increments a counter by 1
 %% 
 %% returns ok or {error, Reason}
-increment(Key, Samplerate) ->
-	count(Key, 1, Samplerate).
-increment(Key) ->
-	count(Key, 1).
+increment(State, Key, Samplerate) ->
+	count(State, Key, 1, Samplerate).
+increment(State, Key) ->
+	count(State, Key, 1).
 	
 %% Public: decrements a counter by 1
 %% 
 %% returns ok or {error, Reason}
-decrement(Key, Samplerate) ->
-	count(Key, -1, Samplerate).
-decrement(Key) ->
-	count(Key, -1).
+decrement(State, Key, Samplerate) ->
+	count(State, Key, -1, Samplerate).
+decrement(State, Key) ->
+	count(State, Key, -1).
 
 %% Public: increments a counter by an arbitrary integer value
 %%
 %% returns: ok or {error, Reason}
-count(Key, Value) ->
-	send({message, Key, Value, c}).
-count(Key, Value, Samplerate) ->
-	send({message, Key, Value, c, Samplerate}).
+count(State, Key, Value) ->
+	send(State, {message, Key, Value, c}).
+count(State, Key, Value, Samplerate) ->
+	send(State, {message, Key, Value, c, Samplerate}).
 
 %% Public: sends a timing in ms
 %%
 %% returns: ok or {error, Reason}
-timing(Key, Value) ->
-	send({message, Key, Value, ms}).
-timing(Key, Value, Samplerate) ->
-	send({message, Key, Value, ms, Samplerate}).
+timing(State, Key, Value) ->
+	send(State, {message, Key, Value, ms}).
+timing(State, Key, Value, Samplerate) ->
+	send(State, {message, Key, Value, ms, Samplerate}).
 	
 %% Internal: prepares and sends the messages
 %%
 %% returns: ok or {error, Reason}
-send(MT) ->
+send(State, MT) ->
 	Message = build_message(MT),
-	send_message(Message).
+	send_message(State, Message).
 
 %% Internal: builds the message string to be sent
 %% 
@@ -51,9 +79,6 @@ build_message({message, Key, Value, Type, Samplerate}) ->
 %% Internal: sends the message over a UDP socket
 %% 
 %% returns: 	
-send_message(Message) ->
+send_message(State, Message) ->
 	io:format("sending message ~p~n", [Message]),
-	
-	{ok, Socket} = gen_udp:open(0),
-	ok = gen_udp:send(Socket, "localhost", 8125, Message),
-	gen_udp:close(Socket).
+	ok = gen_udp:send(State#state.socket, State#state.host, State#state.port, Message).
